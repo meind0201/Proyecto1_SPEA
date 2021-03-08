f2d2191b0486eaceb2db647d31bfbc6a80375a28
MiguelGarcia <miguel_gm16@hotmail.com>
2021-03-08 18:46:12 +0100

    base mqtt

---
+++ src/mqtt/MqttMain.py
@@ -0,0 +1,40 @@
+'''
+Created on 8 mar. 2021
+
+@author: migue
+'''
+
+import paho.mqtt.client as mqtt
+
+import time
+
+# The callback for when the client receives a CONNACK response from the server.
+def on_connect(client, userdata, flags, rc):
+    print("Connected with result code "+str(rc))
+
+    # Subscribing in on_connect() means that if we lose the connection and
+    # reconnect then subscriptions will be renewed.
+    client.subscribe("hola")
+
+# The callback for when a PUBLISH message is received from the server.
+def on_message(client, userdata, msg):
+    print(msg.topic+" "+str(msg.payload))
+
+client = mqtt.Client()
+client.on_connect = on_connect
+client.on_message = on_message
+
+client.username_pw_set("public","public")
+
+client.connect("public.cloud.shiftr.io", 1883, 60)
+
+# Si quiero que esté escuchando para siempre:
+# client.loop_forever()
+# http://www.steves-internet-guide.com/loop-python-mqtt-client/
+
+# Inicia una nueva hebra
+client.loop_start()
+
+
+if __name__ == '__main__':
+    pass
